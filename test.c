fcb713c36559ba8b789990924be3082b0e720c0c
sboof911 <amaach@student.1337.ma>
2021-11-05 12:06:02 +0100

    s

--- test.c
+++
@@ -1,9 +0,0 @@
-#include <sys/time.h>
-
-int     main()
-{
-    struct timeval current_time;
-    gettimeofday(&current_time, NULL);
-    printf("seconds : %ld\nmicro seconds : %ld",
-    current_time.tv_sec, current_time.tv_usec);
-}
